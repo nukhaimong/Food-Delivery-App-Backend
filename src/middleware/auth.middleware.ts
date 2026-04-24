5e9430b68f047dc85384b98b590726d01824dcf7
nukhaimong <nukhaimongmarma834@gmail.com>
2026-04-24 11:31:09 +0600

    update auth middleware: recieved session token from headers

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,27 +1,60 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserRole, UserStatus } from '../types/index.js';
-import { auth as betterAuth } from '../lib/auth.js';
+import { prisma } from '../lib/prisma.js';
 
 const auth = (...roles: UserRole[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const session = await betterAuth.api.getSession({
-        headers: req.headers as any,
-      });
-      if (!session || !session.user) {
+      // const session = await betterAuth.api.getSession({
+      //   headers: req.headers as any,
+      // });
+      // if (!session || !session.user) {
+      //   return res.status(401).json({
+      //     success: false,
+      //     message: 'You are not authenticated',
+      //   });
+      // }
+
+      const authHeader = req.headers.authorization;
+      let sessionToken;
+
+      if (authHeader && authHeader.startsWith('Bearer ')) {
+        sessionToken = authHeader.split(' ')[1];
+      }
+      if (!sessionToken) {
         return res.status(401).json({
           success: false,
-          message: 'You are not authenticated',
+          message: 'Unauthorized access. No token provided. Please login.',
+        });
+      }
+      const isSessionExist = await prisma.session.findFirst({
+        where: {
+          token: sessionToken,
+          expiresAt: {
+            gt: new Date(),
+          },
+        },
+        include: {
+          user: true,
+        },
+      });
+
+      if (!isSessionExist) {
+        return res.status(401).json({
+          success: false,
+          message: 'Session expired or invalid. Please login again.',
         });
       }
 
+      const user = isSessionExist.user;
+
       req.user = {
-        id: session.user.id,
-        name: session.user.name,
-        email: session.user.email,
-        user_role: session.user.user_role as UserRole,
-        image: session.user.image || undefined,
-        status: session.user.status as UserStatus,
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        user_role: user.user_role as UserRole,
+        image: user.image || undefined,
+        status: user.status as UserStatus,
       };
 
       if (roles.length && !roles.includes(req.user.user_role)) {
